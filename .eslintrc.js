d0d1f1e4b62c8e6f3cda41c0aa9ed89bcb30baa1
jossdz <carloscorrea.dev@gmail.com>
2020-08-01 17:08:39 -0500

    ts/eslint/pettier/husky config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    // Prettier plugin and recommended rules
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+
+    'react/prop-types': 'off',
+  },
+  env: {
+    node: true,
+    browser: 'true',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Component } from 'react'
+
+const index: React.FC = () => {
+  return <div>ert</div>
+}
+
+export default index
